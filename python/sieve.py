e5897b954aea33557a2003316d1a78acdc243ee1
mitty1293 <k.fmichi@gmai.com>
2021-08-02 01:33:06 +0900

    実装

--- python/sieve.py
+++ python/sieve.py
@@ -1,27 +1,37 @@
 import sys, math
-from decimal import Decimal, ROUND_HALF_UP, ROUND_HALF_EVEN
 
 def main(input_value: str):
     if validate(input_value):
-        sieve(int(input_value))
-        print("sieve")
+        print('Prime List:', sieve(int(input_value)))
         return
     print("Use an integer number (2 or more) as an argument.")
     return
 
 def validate(data: str) -> bool:
     try:
-        int_data = int(data)
+        int_data: int = int(data)
     except ValueError:
         return False
     if int_data < 2:
         return False
     return True
 
-def sieve(count: int):
-    natural: list = [True] * count
-    natural[0] = False
-    print(count)
+def sieve(count: int) -> list:
+    natural: list = [True] * (count + 1)
+    natural[1] = False
+    prime: list = []
+    
+    i: int = 2
+    while i < math.sqrt(count):
+        if natural[i]:
+            prime.append(i)
+            for j in range(i**2, count+1, i):
+                natural[j] = False
+        i += 1
+    for k in range(i, count+1):
+        if natural[k]:
+            prime.append(k)
+    return prime
 
 if __name__=='__main__':
     main(sys.argv[1])
