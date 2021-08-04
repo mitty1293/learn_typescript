1decb050f7275d5346cb8114e954573b416bbf7b
mitty1293 <k.fmichi@gmai.com>
2021-08-04 09:17:30 +0900

    fix

--- TypeScript/src/ts/sieve.ts
+++ TypeScript/src/ts/sieve.ts
@@ -2,10 +2,13 @@ export const sieve = (count: int): number[] => {
     const natural: boolean[] = Array(count+1).fill(true).fill(false, 0, 2);
     const prime: boolean[] = [];
 
-    for (let i = 2, i < Math.sqrt(count), i++) {
+    for (let i = 2; i < Math.sqrt(count); i++) {
         if (natural[i]) {
             prime.push(i);
-            for (let j = i**2, i <= (count+1), )
+            for (let j = i**2; j <= (count+1); j+=i) {
+                natural[j] = false;
+            }
         }
     }
+    for (let k = )
 };
