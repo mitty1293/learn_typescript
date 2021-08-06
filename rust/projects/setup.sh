9faffee17b81bd4f60dab5eb56b7f6bdc09ccfbb
mitty1293 <k.fmichi@gmai.com>
2021-08-06 12:34:53 +0900

    fix

--- rust/projects/setup.sh
+++ rust/projects/setup.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 # useraddすると[uid:1000],[gid:1000]でユーザ作成されるため別でid設定は不要
 useradd -s /bin/bash -u ${USER_ID} ${USER_NAME}
-groupadd -g ${GROUP_ID} ${GROUP_NAME}
-usermod -g ${GROUP_NAME} ${USER_NAME}
 su ${USER_NAME}
-
-
-# https://tech-blog.rakus.co.jp/entry/20200826/docker
